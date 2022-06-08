79331f1d89932b7f76e2557dced11f5348cde979
kc17m <64145266+kc17m@users.noreply.github.com>
2022-06-08 13:20:26 +0200

    success message amend

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -5,7 +5,9 @@ const GameOver = ({ turns, shuffleCards }) => {
     <div className="game-over">
       <h2>Game Over!</h2>
       {turns <= 10 && <h5>Pretty good! It took you {turns} turns</h5>}
-      {turns <= 15 && <h5>Not too bad, you needed {turns} rounds</h5>}
+      {turns > 10 && turns <= 15 && (
+        <h5>Not too bad, you needed {turns} rounds</h5>
+      )}
       {turns > 15 && (
         <h5>Come on, you can do better. You needed {turns} attempts</h5>
       )}
